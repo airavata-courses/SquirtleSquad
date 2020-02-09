305975981af8652887e12eda9057e87e5dfe4140
CleverParty <shanatmail@gmail.com>
2020-02-09 06:26:16 +0530

    user profile

--- API_a2b/main.go
+++ API_a2b/main.go
@@ -11,13 +11,13 @@ import (
 
 	"github.com/go-kit/kit/log"
 	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
-	httptransport "github.com/go-kit/kit/transport/http"
+	trans "github.com/go-kit/kit/transport/http"
 )
 
 func main() {
 	var (
-		listen = flag.String("listen", ":8080", "HTTP listen address")
-		proxy  = flag.String("proxy", "", "Optional comma-separated list of URLs to proxy uppercase requests")
+		listen = flag.String("listen", ":8081", "HTTP listen address")
+		proxy  = flag.String("proxy", "", "Enter details here")
 	)
 	flag.Parse()
 
@@ -27,19 +27,19 @@ func main() {
 
 	fieldKeys := []string{"method", "error"}
 	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
-		Namespace: "my_group",
+		Namespace: "test_group",
 		Subsystem: "string_service",
 		Name:      "request_count",
 		Help:      "Number of requests received.",
 	}, fieldKeys)
 	requestLatency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-		Namespace: "my_group",
+		Namespace: "test_group",
 		Subsystem: "string_service",
 		Name:      "request_latency_microseconds",
 		Help:      "Total duration of requests in microseconds.",
 	}, fieldKeys)
 	countResult := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-		Namespace: "my_group",
+		Namespace: "test_group",
 		Subsystem: "string_service",
 		Name:      "count_result",
 		Help:      "The result of each count method.",
@@ -51,19 +51,14 @@ func main() {
 	svc = loggingMiddleware(logger)(svc)
 	svc = instrumentingMiddleware(requestCount, requestLatency, countResult)(svc)
 
-	uppercaseHandler := httptransport.NewServer(
-		makeUppercaseEndpoint(svc),
-		decodeUppercaseRequest,
-		encodeResponse,
-	)
-	countHandler := httptransport.NewServer(
+
+	count := trans.NewServer(
 		makeCountEndpoint(svc),
 		decodeCountRequest,
 		encodeResponse,
 	)
 
-	http.Handle("/uppercase", uppercaseHandler)
-	http.Handle("/count", countHandler)
+	http.Handle("/count", count)
 	http.Handle("/metrics", promhttp.Handler())
 	logger.Log("msg", "HTTP", "addr", *listen)
 	logger.Log("err", http.ListenAndServe(*listen, nil))
