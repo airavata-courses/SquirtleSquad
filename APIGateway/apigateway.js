6f05969a569753c12a16c2121c4251e8eb5a519c
satvenk <satvenk@iu.edu>
2020-02-10 20:40:50 -0500

    sess2api

--- APIGateway/apigateway.js
+++ APIGateway/apigateway.js
@@ -7,9 +7,11 @@ const app = express();
 const router = express.Router();
 
 const topic = 'apigateway';
-const client = new kafka.client('localhost:9092');
-const producer = new kafka.HighLevelProducer(client);
-const consumer = new kafka.HighLevelConsumer(client, {topics:topic});
+const Producer = kafka.Producer,
+      Consumer = kafka.Consumer,
+      client = new kafka.KafkaClient(),
+      producer = new Producer(client),
+      consumer = new Consumer(client,[{ topic: 'test'}],{autoCommit: true});
 
 producer.on('ready', function(){
     //Create a message
@@ -18,29 +20,22 @@ producer.on('ready', function(){
         topic: topic,
         message: msg,
     }];  
-    producer.send(payLoad, function(err, res){
-        if(err){
-            console.log(err);
-        } else {
-            console.log("Sent payload to kafka"+" "+msg);
-        }
-    });
 });
 
 producer.on('error', function(err){
     console.log(err);
 });
 
-consumer.on()
+
 
 //BodyParser
 app.use(express.urlencoded({extended: false}));
 
 //Routes
-app.use('/dataRetrieval', require('./apis/dataRetrieval'));
+//app.use('/dataRetrieval', require('./apis/dataRetieval'));
 app.use('/sessionManagement', require('./apis/sessionManagement'));
-app.use('/ModelExecution', require('./apis/modelExec'));
-app.use('/Inference', require('.apis/inference'));
+//app.use('/ModelExecution', require('./apis/modelExec'));
+//app.use('/Inference', require('.apis/inference'));
 
 
 
@@ -49,4 +44,3 @@ const PORT = process.env.PORT || 8082;
 
 app.listen(PORT, console.log(`Server at port ${PORT}`));
 
-module.exports = router;
