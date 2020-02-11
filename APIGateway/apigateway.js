860b936fb2fbb45cf1c574b4eeb6a4af9d7407cc
anuragkumar95 <anukr95@gmail.com>
2020-02-10 20:38:55 -0500

    Created prod, cons on APIGateway

--- APIGateway/apigateway.js
+++ APIGateway/apigateway.js
@@ -5,11 +5,17 @@ const kafka = require('kafka-node');
 
 const app = express();
 const router = express.Router();
-
 const topic = 'apigateway';
-const client = new kafka.client('localhost:9092');
-const producer = new kafka.HighLevelProducer(client);
-const consumer = new kafka.HighLevelConsumer(client, {topics:topic});
+const client = new kafka.KafkaClient();
+const Producer = kafka.HighLevelProducer;
+//const Consumer = kafka.Consumer;
+const producer = new Producer(client);
+//let consumer = new Consumer(client, topic);
+
+Consumer = kafka.Consumer,
+//client = new kafka.KafkaClient(),
+//producer = new Producer(client),
+consumer = new Consumer(client,[{ topic: topic}],{autoCommit: true});
 
 producer.on('ready', function(){
     //Create a message
@@ -29,18 +35,31 @@ producer.on('ready', function(){
 
 producer.on('error', function(err){
     console.log(err);
+});bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test_result
+
+consumer.on('message', function (message){
+    var msg = message.value;
+    console.log(msg);
 });
 
-consumer.on()
+consumer.on('error', function(err){
+    console.log(err)
+});
+
+process.on('SIGINT', function() {
+    consumer.close(true, function() {
+      process.exit();
+    });
+  });
 
 //BodyParser
 app.use(express.urlencoded({extended: false}));
 
 //Routes
-app.use('/dataRetrieval', require('./apis/dataRetrieval'));
-app.use('/sessionManagement', require('./apis/sessionManagement'));
-app.use('/ModelExecution', require('./apis/modelExec'));
-app.use('/Inference', require('.apis/inference'));
+//app.use('/dataRetrieval', require('./apis/dataRetrieval'));
+//app.use('/sessionManagement', require('./apis/sessionManagement'));
+//app.use('/ModelExecution', require('./apis/modelExec'));
+//app.use('/Inference', require('.apis/inference'));
 
 
 
