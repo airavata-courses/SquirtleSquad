432837ff5ac589710fccf746b4a7ca7fae71c198
anuragkumar95 <anukr95@gmail.com>
2020-05-02 23:58:43 -0400

    test.sh file changed

--- Kubes2/test.sh
+++ Kubes2/test.sh
@@ -1,10 +1,10 @@
 kubectl delete deployment,svc --all
 kubectl apply -f <(istioctl kube-inject -f message.yml)
 kubectl apply -f <(istioctl kube-inject -f apigateway.yml)
-kubectl delete ctmd-config
+kubectl delete secret ctmd-config
 kubectl create secret generic ctmd-config --from-literal="MongoDB__ConnectionString=mongodb+srv://sathyan:1234@sathyan-cluster-jj8km.mongodb.net/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 kubectl apply -f <(istioctl kube-inject -f sessionmanagement.yml)
-kubectl delete ctmd-config
+kubectl delete secret ctmd-config
 kubectl create secret generic ctmd-config --from-literal=MongoDB__ConnectionString="mongodb+srv://anuragkumar:Qwerty@123@cluster0-mbssp.mongodb.net/test?retryWrites=true&w=majority"
 kubectl apply -f <(istioctl kube-inject -f usermanagement.yml)
 kubectl apply -f <(istioctl kube-inject -f modelexecution.yml)
