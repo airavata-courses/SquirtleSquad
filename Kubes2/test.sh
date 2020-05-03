7fba04f9d7b7c9b65145434d64b71e188f5655ee
maxprime1 <sathyan.1997@gmail.com>
2020-05-03 03:47:42 +0000

    test file changed

--- Kubes2/test.sh
+++ Kubes2/test.sh
@@ -1,3 +1,9 @@
-kubectl delete deployment,svc modelexecution
-kubectl apply -f modelexecution.yml 
+kubectl delete deployment,svc --all
+kubectl apply -f <(istioctl kube-inject -f message.yml)
+kubectl apply -f <(istioctl kube-inject -f apigateway.yml)
+kubectl apply -f <(istioctl kube-inject -f sessionmanagement.yml)
+kubectl apply -f <(istioctl kube-inject -f usermanagement.yml)
+kubectl apply -f <(istioctl kube-inject -f modelexecution.yml)
+kubectl apply -f <(istioctl kube-inject -f dataretrieval.yml)
+kubectl apply -f <(istioctl kube-inject -f inference.yml)
 kubectl get pods
