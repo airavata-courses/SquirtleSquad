38507124930a21c2f141d01459d508a11bea7a1a
anuragkumar95 <anukr95@gmail.com>
2020-05-03 00:17:43 -0400

    test.sh file changed

--- Kubes2/test.sh
+++ Kubes2/test.sh
@@ -4,9 +4,9 @@ kubectl apply -f gateway.yml
 kubectl apply -f virtualService.yml
 kubectl apply -f <(istioctl kube-inject -f message.yml)
 kubectl apply -f <(istioctl kube-inject -f apigateway.yml)
-kubectl create secret generic ctmd-config --from-literal="MongoDB__ConnectionString=mongodb+srv://sathyan:1234@sathyan-cluster-jj8km.mongodb.net/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
+kubectl create secret generic ctmd-config --from-literal=MongoDB__ConnectionString="mongodb+srv://sathyan:1234@sathyan-cluster-jj8km.mongodb.net/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 kubectl apply -f <(istioctl kube-inject -f sessionmanagement.yml)
-kubectl create secret generic ctmd-config --from-literal="MongoDB__ConnectionString=mongodb+srv://anuragkumar:Qwerty@123@cluster0-mbssp.mongodb.net/test?retryWrites=true&w=majority"
+kubectl create secret generic ctmd-config --from-literal=MongoDB__ConnectionString="mongodb+srv://anuragkumar:Qwerty@123@cluster0-mbssp.mongodb.net/test?retryWrites=true&w=majority"
 kubectl apply -f <(istioctl kube-inject -f usermanagement.yml)
 kubectl apply -f <(istioctl kube-inject -f modelexecution.yml)
 kubectl apply -f <(istioctl kube-inject -f dataretrieval.yml)
