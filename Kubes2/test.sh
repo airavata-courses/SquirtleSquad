3ec97902736e75e5ecf16828d880ed669263517d
anuragkumar95 <anukr95@gmail.com>
2020-05-03 02:05:14 -0400

    Changed session and user management yml files

--- Kubes2/test.sh
+++ Kubes2/test.sh
@@ -1,9 +1,4 @@
 kubectl delete deployment,svc --all
-kubectl create secret generic ctmd-config-session --from-literal=MongoDB__ConnectionStringSession="mongodb+srv://sathyan:1234@sathyan-cluster-jj8km.mongodb.net/test?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
-kubectl create secret generic ctmd-config-user --from-literal=MongoDB__ConnectionStringUser="mongodb+srv://anuragkumar:Qwerty@123@cluster0-mbssp.mongodb.net/test?retryWrites=true&w=majority"
-kubectl apply -f serviceEntry.yml
-kubectl apply -f gateway.yml
-kubectl apply -f virtualService.yml
 kubectl apply -f <(istioctl kube-inject -f message.yml)
 kubectl apply -f <(istioctl kube-inject -f apigateway.yml)
 kubectl apply -f <(istioctl kube-inject -f sessionmanagement.yml)
